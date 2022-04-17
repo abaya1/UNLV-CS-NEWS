e2b04865b5f6cb82b969bded99abd8eea0e83480
Charisse Calma <cherrie@Charisse-Calmas-Mac-mini.local>
2022-04-16 21:34:41 -0700

    login update

--- Frontend/src/containers/Login/Login.jsx
+++ Frontend/src/containers/Login/Login.jsx
@@ -1,8 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.css';
 import {UNLV, Icon, Password} from './imports.js';
+import axios from 'axios';
 
-const Login = () => {
+
+
+
+
+
+const Login =  () => {
+
+    const[password, setPassword] = useState('');
+    const[user, setUser] = useState('');
+
+    const loginHelper = async (e) =>
+    {
+    e.preventDefault();
+    const thing = await axios.get('/getadminspassword');
+
+    if(thing.data.password == password && thing.data.username == user)
+    {
+       // redirect to  admin dashboard 
+    }
+    else
+    {
+        //display error message
+    }
+
+
+    return thing
+    }
+
+
+
     return (
         <div className="login">
             <img src= {UNLV} alt="UNLV logo"/>
@@ -10,14 +40,14 @@ const Login = () => {
                 <h1>Welcome Back</h1>
                 <p>Enter credentials to access admin account</p>
                 <div className="login__value">
-                    <form>
+                    <form onSubmit={loginHelper}>
                         <div className="login__username">
                             <img src={Icon} />
-                            <input type="text" placeholder="Username" />
+                            <input type="text" placeholder="Username" value={user} onChange={(e) => setUser(e.target.value)}/>
                         </div>
                         <div className="login__password">
                             <img src={Password} />
-                            <input type="password" placeholder='Password' />
+                            <input type="password" placeholder='Password' value={password}  onChange={(e) => setPassword(e.target.value)} />
                         </div>
                         <input type="submit" value="Sign In"/>
                     </form>
