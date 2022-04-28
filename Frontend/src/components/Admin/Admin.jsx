da272268e7a9438c0bc7faeadc5d7a1c40e54fc0
Charisse <charissecalma05@gmail.com>
2022-04-28 01:09:41 -0700

    Admin Dashboard

--- Frontend/src/components/Admin/Admin.jsx
+++ Frontend/src/components/Admin/Admin.jsx
@@ -3,10 +3,8 @@ import './Admin.css';
 
 const Admin = ({title, color}) => {
     return (
-        <div className="admin__container">
-            <div className={color}>
-                <h1>{title}</h1>
-            </div>
+        <div className={`btn-grad ${color}`}>
+            <h1>{title}</h1>
         </div>
     );
 };
--- Frontend/src/containers/AdminDashboard/AdminDashboard.jsx
+++ Frontend/src/containers/AdminDashboard/AdminDashboard.jsx
@@ -5,14 +5,18 @@ import './AdminDashboard.css';
 const AdminDashboard = () => {
     return(
         <div className="dashboard">
-            <div className="dashboard__line"></div>
-            <h1>Dashboard</h1>
+            <div className="dashboard__header">
+                <h1 className="dashboard__title">Dashboard</h1>
+            </div>
             <div className="dashboard__selection">
                 <div className="dashboard__single">
                     <Admin title="Add" color="red"/>
                 </div>
                 <div className="dashboard__single">
                     <Admin title="Edit" color="blue"/>
+                </div>
+                <div className="dashboard__single">
+                    <Admin title="Delete" color="black"/>
                 </div>
             </div>
         </div>
