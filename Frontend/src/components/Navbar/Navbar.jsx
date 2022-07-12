e6be0859d9cf4d9d33b8f94392abc59927a53a39
Arron Abay <74326460+abaya1@users.noreply.github.com>
2022-07-12 15:59:33 -0700

    Update Navbar.jsx

--- Frontend/src/components/Navbar/Navbar.jsx
+++ Frontend/src/components/Navbar/Navbar.jsx
@@ -3,6 +3,16 @@ import Logo from '../../assets/logo.png';
 import './Navbar.css';
 
 const Navbar = ({filled, text}) => {
+    if(text == "ADMIN LOGIN")
+    {
+        return (
+        <div className="navbar__container">
+        <Link to="/logAdmin" className={filled}>{text}</Link>
+        </div>
+        );  
+    }
+    
+    
     return (
         <div className="navbar__container">
             <a href="#" className={filled}>{text}</a>
